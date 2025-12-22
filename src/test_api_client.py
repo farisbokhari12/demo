99c1a86eb98e1d791d5bce4d8c851b224efa6ae7
Faris Bokhari <farisbokhari12@gmail.com>
2025-12-21 20:06:07 -0800

    Mock requests.request in test_new_feature to avoid external HTTP call

--- src/test_api_client.py
+++ src/test_api_client.py
@@ -11,6 +11,20 @@ def api_client():
     return UserAPIClient(base_url='http://example.com', api_key='test')
 
 
-def test_new_feature(api_client):
+def test_new_feature(api_client, monkeypatch):
+    class DummyResponse:
+        def __init__(self):
+            self.status_code = 200
+            self.url = api_client.base_url + '/new-feature'
+        def raise_for_status(self):
+            return None
+        def json(self):
+            return {'status': 'success'}
+
+    def fake_request(method, url, headers=None, timeout=None, **kwargs):
+        return DummyResponse()
+
+    monkeypatch.setattr('requests.request', fake_request)
+
     response = api_client.new_feature({'key': 'value'})
     assert response['status'] == 'success'
